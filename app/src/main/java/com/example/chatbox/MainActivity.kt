bcdc3675008474d0e56612e7494d1fbd346446ac
Mahmoud Tarek <dev.mahmoudtarek@gmail.com>
2024-05-23 13:53:46 +0200

    enhancement over splash screen

--- app/src/main/java/com/example/chatbox/MainActivity.kt
+++ app/src/main/java/com/example/chatbox/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.chatbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.chatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Handle the splash screen transition.
-        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Binding to Access all the views
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
