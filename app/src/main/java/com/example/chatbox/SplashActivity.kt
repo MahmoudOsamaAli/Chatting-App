7f6a4c97ad2d9a0991b042ace2664015cf043e25
ma.osama <ma.osama@qoad.com>
2024-12-25 16:45:53 +0200

    enhancements

---
+++ app/src/main/java/com/example/chatbox/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.chatbox
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.chatbox.auth.startupActivity.StartupActivity
+import com.example.chatbox.databinding.ActivitySplashBinding
+import com.example.chatbox.main.MainActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { true }
+        super.onCreate(savedInstanceState)
+        // Declare Binding to Access all the views
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        val currentUser = Firebase.auth.currentUser
+        MainScope().launch {
+            delay(2000)
+            if (currentUser == null) {
+                startActivity(Intent(this@SplashActivity, StartupActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+            finish()
+
+        }
+    }
+}
--- app/src/main/java/com/example/chatbox/auth/startupActivity/StartupActivity.kt
+++ app/src/main/java/com/example/chatbox/auth/startupActivity/StartupActivity.kt
@@ -4,80 +4,106 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.example.chatbox.auth.GoogleAuthHelper
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import com.example.chatbox.R
 import com.example.chatbox.auth.login.LoginActivity
-import com.example.chatbox.auth.signup.SignupActivity
 import com.example.chatbox.databinding.ActivityStartupBinding
 import com.example.chatbox.main.MainActivity
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
+@Suppress("DEPRECATION")
 class StartupActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "StartupActivity"
+    }
+    private val REQ_ONE_TAP = 2
     private lateinit var binding: ActivityStartupBinding
-    private lateinit var googleAuthHelper: GoogleAuthHelper
+    private lateinit var auth: FirebaseAuth
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
+        // Declare Binding to Access all the views
         binding = ActivityStartupBinding.inflate(layoutInflater)
+        // Change the StatusBar Title color to White
+        val windowInsetController = ViewCompat.getWindowInsetsController(window.decorView)
+        windowInsetController?.isAppearanceLightStatusBars = false
+        // Handle the splash screen transition.
+        installSplashScreen()
+        // hide ActionBar
+        actionBar?.hide()
         setContentView(binding.root)
-
-        supportActionBar?.hide()
-
-        googleAuthHelper = GoogleAuthHelper(this)
-
-        // Google Sign-In button listener
-        binding.googleLogin.setOnClickListener {
-            googleAuthHelper.startGoogleSignIn(googleSignInLauncher)
-        }
-
-        // Check user status
-        checkUserStatus()
-
+        // switching to signup Activity
+        auth = Firebase.auth
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            val signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(
+                    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build()
+                )
+                .build()
         }
 
-        binding.BtnSignUp.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
-        }
     }
 
-    // Register for Google Sign-In result
-    private val googleSignInLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            googleAuthHelper.handleSignInResult(
-                result.resultCode,
-                result.data,
-                onSuccess = { idToken ->
-                    googleAuthHelper.signInWithFirebase(idToken,
-                        onSuccess = { user ->
-                            Log.d("Firebase Sign-In", "Successfully signed in with Firebase: ${user?.email}")
-                            navigateToMain(user)
-                        },
-                        onFailure = { error ->
-                            Log.e("Firebase Sign-In", error)
-                        })
-                },
-                onFailure = { error ->
-                    Log.e("Google Sign-In", error)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+//                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = "credential.googleIdToken"
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                            auth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this) { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithCredential:success")
+                                        val user = auth.currentUser
+                                        continueWithTheFlow()
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                                    }
+                                }
+                        }
+
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
                 }
-            )
-        }
-
-    private fun checkUserStatus() {
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        if (currentUser != null) {
-            navigateToMain(currentUser)
+            }
         }
     }
 
-    private fun navigateToMain(user: FirebaseUser?) {
+    private fun continueWithTheFlow() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
+
 }
